f41429871fcd299cf0a8ac3f320bd746a6ed54c3
dlondon4 <dlondon4@gmail.com>
2014-11-24 09:47:48 -0600

    Added Category Model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :tasks
   belongs_to :user
+
+  validates :name, :presence => true
+  validates :user, :presence => true
+  validates :name, :uniqueness => { :scope => :user }
+  validates :user, :uniqueness => { :scope => :order }
 end
