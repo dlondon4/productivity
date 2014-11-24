c7cdc97b7918bc72ddd5b1bfa2a6932800b7ed5f
dlondon4 <dlondon4@gmail.com>
2014-11-24 09:59:30 -0600

    Added a comment to remind me for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true :uniqueness => true
+  # Add one for validating a unique first_name last_name and email combination
 end
